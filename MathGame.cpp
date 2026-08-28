466fe2112ab2444929e2bf959141d5356530753c
ahmed <ahmed.mansourr.afify@gmail.com>
2026-08-28 20:28:28 +0300

    add func

--- MathGame.cpp
+++ MathGame.cpp
@@ -30,6 +30,11 @@ struct  stQuizz
 
 };
 
+void ClearScreen()
+{
+    system("clear");
+}
+
 int RandomNumber(int From, int To)
 {
     return rand() % (To - From + 1) + From;
@@ -130,10 +135,10 @@ int SimpleCalculator(int Number1, int Number2, enOperationType OpType)
 void PrintQuestion(stQuizz &Quizz, short NumberOfQuestion)
 {
     cout << "\n";
-    cout << "Question [" << NumberOfQuestion + 1 << "/" << Quizz.QuestionsList << "] \n\n";
+    cout << "Question [" << NumberOfQuestion + 1 << "/" << Quizz.NumberOfQuestion << "] \n\n";
     cout << Quizz.QuestionsList[NumberOfQuestion].Number1 << endl;
-    cout << Quizz.QuestionsList[NumberOfQuestion].Number2 << endl;
-    cout << GetOpTypeSymbol(Quizz.OperationType);
+    cout << Quizz.QuestionsList[NumberOfQuestion].Number2 << " ";
+    cout << GetOpTypeSymbol(Quizz.QuestionsList[NumberOfQuestion].OpType);
     cout << "\n---------------" << endl;
 
 }
@@ -160,21 +165,21 @@ stQuestion GenerateQuestion(enOperationType OpType, enQuestionLevel QuesetionLvl
     case  enQuestionLevel::Easy:
         Question.Number1 = RandomNumber(1, 10);
         Question.Number2 = RandomNumber(1, 10);
-        Question.CorrectAnswer = SimpleCalculator(Question.Number1, Question.Number2, OpType);
+        Question.CorrectAnswer = SimpleCalculator(Question.Number1, Question.Number2, Question.OpType);
         return Question;
         break;
 
     case  enQuestionLevel::Medium:
         Question.Number1 = RandomNumber(10, 50);
         Question.Number2 = RandomNumber(10, 50);
-        Question.CorrectAnswer = SimpleCalculator(Question.Number1, Question.Number2, OpType);
+        Question.CorrectAnswer = SimpleCalculator(Question.Number1, Question.Number2, Question.OpType);
         return Question;
         break;    
     
         case  enQuestionLevel::Hard:
         Question.Number1 = RandomNumber(50, 100);
         Question.Number2 = RandomNumber(50, 100);
-        Question.CorrectAnswer = SimpleCalculator(Question.Number1, Question.Number2, OpType);
+        Question.CorrectAnswer = SimpleCalculator(Question.Number1, Question.Number2, Question.OpType);
         return Question;
         break;
    
@@ -189,6 +194,44 @@ void GenerateQuizzQuestioins(stQuizz &Quizz)
     {
         Quizz.QuestionsList[Question] = GenerateQuestion(Quizz.OperationType, Quizz.QuestionsLevel);
     }
+}
+
+void CorrectTheQuestionAnswer(stQuizz &Quizz, short QuistionNumber)
+{
+    if(Quizz.QuestionsList[QuistionNumber].PlayerAnswer != Quizz.QuestionsList[QuistionNumber].CorrectAnswer)
+    {
+        Quizz.QuestionsList[QuistionNumber].AnswerResult = false;
+        Quizz.NumberOfWrongAnswers++;
+
+        cout << "Wrong Answer -:(\n";
+        cout << "The Correct Answer Is:\n";
+        cout << Quizz.QuestionsList[QuistionNumber].CorrectAnswer;
+        cout << "\n";
+    }
+    else
+    {
+        Quizz.QuestionsList[QuistionNumber].CorrectAnswer = true;
+        Quizz.NumberOfCorrectAnswers++;
+
+        cout << "Correct Answer -:)\n";
+    }
+
+    cout << endl;
+
+    
+}
+
+void AskAndCorrectQuestionList(stQuizz &Quizz)
+{
+    for(short Question = 0; Question < Quizz.NumberOfQuestion; Question++)
+    {
+        PrintQuestion(Quizz, Question);
+        Quizz.QuestionsList[Question].PlayerAnswer = ReadQuestionAnswer();
+        CorrectTheQuestionAnswer(Quizz, Question);
+
+    }
+
+    Quizz.isPass = (Quizz.NumberOfCorrectAnswers >= Quizz.NumberOfWrongAnswers);
 }
 
 int main()
